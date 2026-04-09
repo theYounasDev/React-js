07d717ec19471d280f53f2d0421dca6dadb5802d
theYounasDev <younasdeveloper61@gmail.com>
2026-04-09 10:39:06 +0500

    02-components added

---
+++ 02-componenets/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Card from './componenets/card'
+import Navbar from './componenets/navbar'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Card />
+    </div>
+  )
+}
+
+export default App
---
+++ 02-componenets/src/componenets/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='card'>
+        <h1>younas khan</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, quidem?</p>
+    </div>
+  )
+}
+
+export default Card
